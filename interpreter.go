a0840be831be294e516b9bff46027ffe3bffb28f
Kunal Kundu <kunal99kundu@gmail.com>
2021-07-21 02:29:58 +0530

    adds operators to move pointers and change values

--- interpreter.go
+++ interpreter.go
@@ -3,16 +3,20 @@ package main
 import "errors"
 
 type Interpreter struct {
-	source []byte
-	cells  []byte
+	source   []byte
+	cells    []byte
+	position int
 }
+
+const DEFAULT_CELLS = 30000
 
 var ErrInvalidSize = errors.New("max cells must be greater than 1")
 
 func New(source []byte) *Interpreter {
 	return &Interpreter{
-		source: source,
-		cells:  make([]byte, 30),
+		source:   source,
+		cells:    make([]byte, DEFAULT_CELLS),
+		position: 0,
 	}
 }
 
@@ -23,5 +27,28 @@ func (in *Interpreter) Run() {
 }
 
 func (in *Interpreter) interpret(cell byte) {
+	switch cell {
+	case '>':
+		if in.position == len(in.cells)-1 {
+			in.position = 0
+		} else {
+			in.position++
+		}
+	case '<':
+		if int(in.position) == 0 {
+			in.position = len(in.cells) - 1
+		} else {
+			in.position--
+		}
+	case '+':
+		in.cells[in.position]++
+	case '-':
+		in.cells[in.position]--
+	case ',':
+	case '.':
+	case '[':
+	case ']':
+
+	}
 
 }
