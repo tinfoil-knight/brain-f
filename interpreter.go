128b6c923b096a82c2cbe92de131e8786ec447c7
Kunal Kundu <kunal99kundu@gmail.com>
2021-07-21 02:15:23 +0530

    adds interpreter struct and utilities

--- interpreter.go
+++ interpreter.go
@@ -1,1 +1,27 @@
 package main
+
+import "errors"
+
+type Interpreter struct {
+	source []byte
+	cells  []byte
+}
+
+var ErrInvalidSize = errors.New("max cells must be greater than 1")
+
+func New(source []byte) *Interpreter {
+	return &Interpreter{
+		source: source,
+		cells:  make([]byte, 30),
+	}
+}
+
+func (in *Interpreter) Run() {
+	for _, cell := range in.source {
+		in.interpret(cell)
+	}
+}
+
+func (in *Interpreter) interpret(cell byte) {
+
+}
--- main.go
+++ main.go
@@ -1,5 +1,42 @@
 package main
 
-func main() {
+import (
+	"bufio"
+	"errors"
+	"fmt"
+	"io/ioutil"
+	"os"
+	"strings"
+)
 
+func main() {
+	args := os.Args[1:]
+	switch len(args) {
+	case 0:
+		startPrompt()
+	case 1:
+		runFile(args[1])
+	default:
+		panic(errors.New("usage: bf <path-to-file>"))
+	}
+}
+
+func startPrompt() {
+	for {
+		reader := bufio.NewReader(os.Stdin)
+		fmt.Print("> ")
+		raw, _ := reader.ReadString('\n')
+		text := strings.TrimSpace(raw)
+		// TODO
+		fmt.Println(text)
+	}
+}
+
+func runFile(path string) {
+	bytes, err := ioutil.ReadFile(path)
+	if err != nil {
+		panic(err)
+	}
+	instance := New(bytes)
+	instance.Run()
 }
